e929717eb1401e10f6e5786fd249318d1219e34c
Ethan-Lyc <1061784200@qq.com>
2023-03-03 14:52:59 +0800

    "init() implemented"

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -2,13 +2,21 @@ package gitlet;
 
 // TODO: any imports you need here
 
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date; // TODO: You'll likely use this in this class
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import static gitlet.Utils.sha1;
 
 /** Represents a gitlet commit object.
  *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
- *  @author TODO
+ *  @author Yicheng Liao
  */
 public class Commit {
     /**
@@ -21,6 +29,51 @@ public class Commit {
 
     /** The message of this Commit. */
     private String message;
+    /** The time of this Commit. */
+    private String timestamp;
+    /** The parent of this Commit. */
+    private List<String> parents;
+    /** The blobs of this Commit. */
+    private Map<String,String> blobs;
+    /** The sha1 of this Commit. */
+    private String Id;
+    /** The File of this Commit. */
+    private File file;
+
+
+    public Commit(String message, Date timestamp, List<String> parents, Map<String,String> blobs, String[] files){
+        this.message = message;
+        this.timestamp = dateToTimeStamp(timestamp);
+        this.parents = parents;
+        this.blobs = blobs;
+        this.Id = sha1(message + timestamp + parents + blobs + files);
+        this.file = new File(".gitlet/objects/" + Id);
+    }
 
     /* TODO: fill in the rest of this class. */
+    public String getMessage(){
+        return message;
+    }
+    public String getTimestamp(){
+        return timestamp;
+    }
+    public List<String> getParents(){
+        return parents;
+    }
+    public Map<String,String> getBlobs(){
+        return blobs;
+    }
+    public String getId(){
+        return Id;
+    }
+    public File getFile(){
+        return file;
+    }
+    public void setMessage(String message){
+        this.message = message;
+    }
+    private static String dateToTimeStamp(Date date) {
+        DateFormat dateFormat = new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy Z", Locale.US);
+        return dateFormat.format(date);
+    }
 }
--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,6 +1,12 @@
 package gitlet;
 
+import org.junit.Test;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
 import static gitlet.Utils.*;
 
 // TODO: any imports you need here
@@ -24,6 +30,27 @@ public class Repository {
     public static final File CWD = new File(System.getProperty("user.dir"));
     /** The .gitlet directory. */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
+    public static final File BRANCHES = join(GITLET_DIR, "branches");
+    public static final File OBJECTS = join(GITLET_DIR, "objects");
 
     /* TODO: fill in the rest of this class. */
+    public static void init(){
+        if(GITLET_DIR.exists()){
+            System.out.println("A gitlet version-control system already exists in the current directory.");
+            System.exit(0);
+        }
+        GITLET_DIR.mkdir();
+        OBJECTS.mkdir();
+        BRANCHES.mkdir();
+        Commit initialCommit = new Commit("initial commit", new Date(0),
+                new ArrayList<>(), new HashMap<>(), null);
+        File initialCommitFile = initialCommit.getFile();
+        writeObject(initialCommitFile, Commit.class);
+        File master = join(BRANCHES, "master");
+        writeContents(master, sha1(initialCommit.toString()));
+    }
+    @Test
+    public void testInit(){
+        init();
+    }
 }
